881527a5697ce330772c4c5e6c8ebacf60eaa784
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2026-04-18 14:47:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,20 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// static files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// routes
 app.use("/auth", require("./routes/auth"));
 app.use("/resume", require("./routes/resume"));
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+// IMPORTANT: use Render port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
