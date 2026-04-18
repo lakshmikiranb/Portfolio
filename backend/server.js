a679445bea795c12057cf23395d11d5717f115a9
lakshmikiranb <111352316+lakshmikiranb@users.noreply.github.com>
2026-04-18 14:49:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// static files
+// serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // routes
 app.use("/auth", require("./routes/auth"));
 app.use("/resume", require("./routes/resume"));
 
-// IMPORTANT: use Render port
+// IMPORTANT: use dynamic port for hosting
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
